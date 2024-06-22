3738deaf12877f23d8c168869ded3835ae693652
igortr17 <igorteixeirar@gmail.com>
2024-06-22 11:28:35 -0300

    Initial commit

--- app/Http/Controllers/TouroController.php
+++ app/Http/Controllers/TouroController.php
@@ -7,20 +7,20 @@ use Illuminate\Http\Request;
 
 class TouroController extends Controller
 {
-    // Lista todos os touros
+    
     public function index()
     {
         $touros = Touro::all();
         return view('touros.index', compact('touros'));
     }
 
-    // Mostra o formulário para criar um novo touro
+    
     public function create()
     {
         return view('touros.create');
     }
 
-    // Salva um novo touro
+    
     public function store(Request $request)
     {
         $request->validate([
@@ -33,19 +33,19 @@ class TouroController extends Controller
         return redirect()->route('touros.index')->with('success', 'Touro criado com sucesso.');
     }
 
-    // Mostra um touro específico
+    
     public function show(Touro $touro)
     {
         return view('touros.show', compact('touro'));
     }
 
-    // Mostra o formulário para editar um touro
+    
     public function edit(Touro $touro)
     {
         return view('touros.edit', compact('touro'));
     }
 
-    // Atualiza um touro específico
+    
     public function update(Request $request, Touro $touro)
     {
         $request->validate([
@@ -58,7 +58,7 @@ class TouroController extends Controller
         return redirect()->route('touros.index')->with('success', 'Touro atualizado com sucesso.');
     }
 
-    // Deleta um touro específico
+    
     public function destroy(Touro $touro)
     {
         $touro->delete();
